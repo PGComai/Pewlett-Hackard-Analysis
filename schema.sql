33d1dce6b63728767807870066ce34585a2bf629
PGComai <comai.alex@gmail.com>
2022-10-29 18:01:36 -0700

    found all those eligible retirees

--- schema.sql
+++ schema.sql
@@ -95,7 +95,33 @@ CREATE TABLE titles (
 
 );
 
-SELECT * FROM departments;
+SELECT * FROM employees;
 
+-- find employees born between two dates
+
+-- retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- retirement eligibility INTO a new table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
 
 
