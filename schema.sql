9a129811dd750c4f21f9993ff79b17e360d52ae2
PGComai <comai.alex@gmail.com>
2022-10-29 19:36:10 -0700

    counted retiring by dept

--- schema.sql
+++ schema.sql
@@ -124,4 +124,16 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
 
+-- remake retirement_info to prepare for join
+
+DROP TABLE retirement_info;
+
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+
 
